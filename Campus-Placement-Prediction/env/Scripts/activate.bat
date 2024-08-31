8e2fbf9d9cd63cd55ca754efaabe53b40386500a
upasanatrivedi <upasanatrivedi62@gmail.com>
2024-08-31 21:10:04 +0530

    uploaded task 3

--- Campus-Placement-Prediction/env/Scripts/activate.bat
+++ Campus-Placement-Prediction/env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\upasa\Desktop\Campus-Placement-Prediction-Task-3\env
+set VIRTUAL_ENV=C:\Users\upasa\Desktop\VerveBridge-ML-Internship\Campus-Placement-Prediction\env
 
 if not defined PROMPT set PROMPT=$P$G
 
